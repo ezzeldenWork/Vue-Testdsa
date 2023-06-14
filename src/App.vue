894a07ffac442b794f3f67f82a8227bc082ee684
ezzelden.work <ezzelden.work@gmail.com>
2023-06-14 16:10:26 +0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TheHeader title="LOL"></TheHeader>
+  <StoredResources :resources="storedResources"> </StoredResources>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TheHeader from "./components/layouts/TheHeader.vue";
+import StoredResources from "./components/learning-resources/StoredResources.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { StoredResources, TheHeader },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "official-guide",
+          title: "Official Guide",
+          description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, fuga.",
+          link: "https://vuejs.org/",
+        },
+        {
+          id: "Google-guide",
+          title: "Google Guide",
+          description:
+            "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Officia, fuga.",
+          link: "https://Google.org/",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Roboto", sans-serif;
+}
+
+body {
+  margin: 0;
 }
 </style>
